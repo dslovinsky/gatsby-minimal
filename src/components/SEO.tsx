192015d4dcdcee9ea586281b3e7c2bdf1ca88e5f
Daniel Slovinsky <slovinsky1993@gmail.com>
2023-08-11 23:56:05 -0700

    docs: add notes to explain purpose of each test

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -7,7 +7,7 @@ interface SEOProps {
 
 const SEO: FC<SEOProps> = ({ title }) => {
   const [hide, setHide] = useState(true);
-  const [googleAdsLoaded, setGoogleAdsLoaded] = useState(false);
+  const [marketoLoadead, setMarketoLoadead] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,26 +19,37 @@ const SEO: FC<SEOProps> = ({ title }) => {
 
   return (
     <>
-      {googleAdsLoaded && <title>{title}</title>}
+      {/* Using state does NOT work outside of loading Script components */}
+      {marketoLoadead && <title>{title}</title>}
       {!hide && (
         <meta name="description" content="Loads 1 second after mount" />
       )}
+      {!hide && (
+        <script async src="https://www.gstatic.com/devrel-devsite/prod/ve7ce216351f398481fccad3cbbc60c699e78bde8533bfe4daa150955665bb2bf/js/analytics_library.js"></script>
+      )}
       <meta name="robots" content={`${hide ? 'no' : ''}index`} />
+      {/* Loading Scripts with state DOES work regardless of how state is changed */}
       {!hide && (
         <Script
+          id="gtm-test"
           src="https://www.googletagmanager.com/ns.html?id=GTM-N5NSCWL"
           onLoad={() => console.log('GTM loaded')}
         />
       )}
+      {/* Conditionally loading off-main-thread Scripts does NOT work */}
       <Script
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        onLoad={() => setGoogleAdsLoaded(true)}
+        id="marketo-test"
+        src="https://munchkin.marketo.net/munchkin.js"
+        onLoad={() => {
+          console.log('marketo loaded');
+          setMarketoLoadead(true);
+        }}
       />
-      {googleAdsLoaded && (
+      {marketoLoadead && (
         <Script
-          id="bizible-test"
-          src="https://cdn.bizible.com/scripts/bizible.js"
-          onLoad={() => console.log('bizible loaded')}
+          id="marketo-test"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          strategy="off-main-thread"
         />
       )}
     </>
